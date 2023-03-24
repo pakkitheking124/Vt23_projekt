b55d846b36bce68c0cd1124a34f409540920ac3a
Pascal Malmquist <04pama@skola.svedala.se>
2023-03-24 13:41:17 +0100

    kuk

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
     include_once ('heads/head.startsida.php');
     include_once ('nav.fot/navbar.php');
 ?>
-
-
-
-
-
+<div class="startsidawrapped">
+    <div class="startwelcome">
+        <h1>Väkommen</h1>
+        <p>Hej och välkommen till PHATOS coffeshop, vi är störst i norden på specialsering av keffe industrin. <br> Vi har allt från caffe
+        maskiner, till kaffe böner i hög kvalitet. <br> Vi tror på en ljus och miljövänlig framtid därför så miljö pakiteras alla våra varor.
+        </p>
+    </div>
+</div>
 
 
 
