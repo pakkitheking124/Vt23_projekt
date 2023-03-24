034567806f2b353387ccca5effbaa963f066b082
Pascal Malmquist <04pama@skola.svedala.se>
2023-03-24 10:53:25 +0100

    footer fix

--- nav.fot/footer.php
+++ nav.fot/footer.php
@@ -1,5 +1,27 @@
+<div class="footer-wrapped">
 
-<p>godmorgon</p>
+    <div class="foot-loga">
+        <img src="../images/loga.png" alt="">
+    </div>
+
+    <div class="foot-contact">
+        <h1>Contact</h1>
+        <a>Gmail: pmalmquist@gmail.com</a>
+        <a>Telfon: 0706424840</a>
+        <a>Börringevägen 10, 233 33 Svedala</a>
+    </div>
+
+    <div class="foot-about">
+        <h1>About</h1>
+        <a>Returnering</a>
+        <a>Någoting</a>
+        <a>Någoting</a>
+    </div>
+
+
+</div>
+
+
 
 </body>
 </html>
